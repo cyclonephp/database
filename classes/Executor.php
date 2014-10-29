18d4e7e59ce74a0a89b58db21c0ff7cc56dfc5a2
Bence Eros <ebence88@gmail.com>
2014-10-29 17:25:28 +0100

    adding new methods to Executor interface, adding Connection interface, adding DatabaseException with appropriate subtypes

--- classes/Executor.php
+++ classes/Executor.php
@@ -1,11 +1,30 @@
 <?php
 namespace cyclonephp\database;
 
-class Executor {
+interface Executor {
     
     /**
+     * @param string $queryString
      * @return QueryResult
      */
     public function execQuery($queryString);
+    
+    /**
+     * @param string $insertStmt
+     * @return int the number if inserted rows
+     */
+    public function execInsert($insertStmt);
+    
+    /**
+     * @param string $updateStmt
+     * @return int the number of updated rows
+     */
+    public function execUpdate($updateStmt);
+    
+    /**
+     * @param string $deleteStmt
+     * @return int the number of deleted rows
+     */
+    public function execDelete($deleteStmt);
     
 }
